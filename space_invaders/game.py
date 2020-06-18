8b7ecbcb742fc59f9575c373cfb881faa4afb5a7
Ray Thurman <raymondthurman5@gmail.com>
2020-06-18 10:19:26 -0400

    reformatted, refactored some of the game class

--- space_invaders/game.py
+++ space_invaders/game.py
@@ -15,38 +15,49 @@ class Game:
         self.enemy = Enemy(300, 100)
         self.player_vel = 5
 
+
     def redraw_window(self):
         WIN.blit(BACKGROUND, (0, 0))
         # Draw text
         lives_label = self.main_font.render(f"Lives: {self.lives}", 1, (255, 255, 255))
         level_label = self.main_font.render(f"Level: {self.level}", 1, (255, 255, 255))
 
+        # Draw the labels
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
 
+        # Draw the Ships
         self.player.draw(WIN)
         self.enemy.draw(WIN)
 
         pygame.display.update()
 
+
     def quit_game(self):
         self.run = False
+
+
+    def check_quit(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.quit_game()
+
+
+    def check_key_press(self, keys):
+        if keys[pygame.K_a] and self.player.x + self.player_vel > 0:  # Left
+            self.player.x -= self.player_vel
+        if keys[pygame.K_d] and self.player.x + self.player_vel + self.player.get_width() < WIDTH:  # Right
+            self.player.x += self.player_vel
+        if keys[pygame.K_w] and self.player.y + self.player_vel > 0:  # Up
+            self.player.y -= self.player_vel
+        if keys[pygame.K_s] and self.player.y + self.player_vel + self.player.get_height() < HEIGHT: # Down
+            self.player.y += self.player_vel
+
 
     def play(self):
         while self.run:
             self.clock.tick(self.FPS)
             self.redraw_window()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.quit_game()
-
+            self.check_quit()
             keys = pygame.key.get_pressed()
-            if keys[pygame.K_a] and self.player.x + self.player_vel > 0:  # Left
-                self.player.x -= self.player_vel
-            if keys[pygame.K_d] and self.player.x + self.player_vel + 50 < WIDTH:  # Right
-                self.player.x += self.player_vel
-            if keys[pygame.K_w] and self.player.y + self.player_vel > 0:  # Up
-                self.player.y -= self.player_vel
-            if keys[pygame.K_s] and self.player.y + self.player_vel + 50 < HEIGHT: # Down
-                self.player.y += self.player_vel
+            self.check_key_press(keys)
--- space_invaders/ship.py
+++ space_invaders/ship.py
@@ -2,7 +2,7 @@ import pygame
 from random import choice
 from constants import *
 
-# Ship class to create Player and Enemy Ships
+# Base Ship class to create Player and Enemy Ships
 class Ship:
     def __init__(self, x, y, health=100):
         self.x = x
@@ -13,11 +13,14 @@ class Ship:
         self.lasers = []
         self.cool_down_counter = 0
 
+
     def draw(self, window):
         window.blit(self.ship_img, (self.x, self.y))
 
+
     def get_width(self):
         return self.ship_img.get_width()
+
 
     def get_height(self):
         return self.ship_img.get_height()
@@ -39,6 +42,7 @@ class Enemy(Ship):
         self.laser_img = self.get_laser()
         self.mask = pygame.mask.from_surface(self.ship_img)
         self.max_health = health
+
 
     def get_laser(self):
         if self.ship_img == RED_SPACE_SHIP:
