6c122fbacbb486909e9db9b535201c5d6f87f677
Raymond Thurman <raymondthurman5@gmail.com>
2020-06-19 11:29:29 -0400

    enemies movement and lives are currently working

--- space_invaders/game.py
+++ space_invaders/game.py
@@ -1,10 +1,13 @@
 import pygame
+import random
 from constants import *
 from ship import Player, Enemy
 
 # Game loop
+
+
 class Game:
-    def __init__(self, run=True, FPS=60, level=1, lives=5):
+    def __init__(self, run=True, FPS=60, level=0, lives=5):
         # Minor game Setup items
         self.run = run
         self.FPS = FPS
@@ -12,6 +15,8 @@ class Game:
         self.lives = lives
         self.main_font = pygame.font.SysFont("comicsans", 50)
         self.clock = pygame.time.Clock()
+        self.lost = False
+        self.loss_count = 0
         # Player items
         self.player = Player(300, 650)
         self.player_vel = 5
@@ -20,12 +25,13 @@ class Game:
         self.wave_length = 5
         self.enemy_vel = 1
 
-
     def redraw_window(self):
         WIN.blit(BACKGROUND, (0, 0))
         # Draw text
-        lives_label = self.main_font.render(f"Lives: {self.lives}", 1, (255, 255, 255))
-        level_label = self.main_font.render(f"Level: {self.level}", 1, (255, 255, 255))
+        lives_label = self.main_font.render(
+            f"Lives: {self.lives}", 1, (255, 255, 255))
+        level_label = self.main_font.render(
+            f"Level: {self.level}", 1, (255, 255, 255))
 
         # Draw the labels
         WIN.blit(lives_label, (10, 10))
@@ -36,19 +42,21 @@ class Game:
             enemy.draw(WIN)
         self.player.draw(WIN)
 
+        if self.lost:
+            lost_lable = self.main_font.render(
+                "You lost!! :(", 1, (255, 255, 255))
+            WIN.blit(lost_lable, (WIDTH/2 - lost_lable.get_width()/2, 350))
+
         # updated the game display
         pygame.display.update()
 
-
     def quit_game(self):
         self.run = False
-
 
     def check_quit(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.quit_game()
-
 
     def check_key_press(self, keys):
         if keys[pygame.K_a] and self.player.x + self.player_vel > 0:  # Left
@@ -57,19 +65,37 @@ class Game:
             self.player.x += self.player_vel
         if keys[pygame.K_w] and self.player.y + self.player_vel > 0:  # Up
             self.player.y -= self.player_vel
-        if keys[pygame.K_s] and self.player.y + self.player_vel + self.player.get_height() < HEIGHT: # Down
+        if keys[pygame.K_s] and self.player.y + self.player_vel + self.player.get_height() < HEIGHT:  # Down
             self.player.y += self.player_vel
-
 
     def play(self):
         while self.run:
             self.clock.tick(self.FPS)
+            self.redraw_window()
+
+            if self.lives <= 0 or self.player.health <= 0:
+                self.lost = True
+                self.loss_count += 1
+
+            if self.lost:
+                if self.loss_count > self.FPS * 3:
+                    self.run = False
+                else:
+                    continue
 
             if len(self.enemies) == 0:
                 self.level += 1
                 self.wave_length += 5
+                for i in range(self.wave_length):
+                    enemy = Enemy(random.randrange(50, WIDTH-50),
+                                  random.randrange(-1500, -100))
+                    self.enemies.append(enemy)
 
             self.check_quit()
             keys = pygame.key.get_pressed()
             self.check_key_press(keys)
-            self.redraw_window()
+            for enemy in self.enemies[:]:
+                enemy.move(self.enemy_vel)
+                if enemy.y + enemy.get_height() > HEIGHT:
+                    self.lives -= 1
+                    self.enemies.remove(enemy)
